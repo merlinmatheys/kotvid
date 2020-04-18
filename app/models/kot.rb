9c5764243bc58b97fc1a0f64a8c4d4a231d74c5d
merlin matheys <merlinmatheys@gmail.com>
2020-04-18 11:16:05 +0200

    A lot of improvement

--- app/models/kot.rb
+++ app/models/kot.rb
@@ -3,4 +3,8 @@ class Kot < ApplicationRecord
   has_many_attached :photos
   geocoded_by :addresse
   after_validation :geocode, if: :will_save_change_to_addresse?
+  validates :addresse, presence: true
+  # validates :type, presence: true
+  validates :quartier, presence: true
+  # validates :nombre_chambres, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
-  resources :kots
+  root to: 'kots#index'
+  resources :kots, except: :index
+
 end
